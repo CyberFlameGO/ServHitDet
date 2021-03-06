07bf0e1529997bf432f3db23e397e47994975ec9
CyberFlame <cyberflameu@gmail.com>
2021-03-07 12:04:49 +1300

    Update Pair.java

--- src/main/java/net/cyberflame/servhitdet/utils/Pair.java
+++ src/main/java/net/cyberflame/servhitdet/utils/Pair.java
@@ -1,7 +1,5 @@
 package net.cyberflame.servhitdet.util;
 
-import java.util.Objects;
-
 public class Pair<K,V> {
 
     private K key;
@@ -27,20 +25,5 @@ public class Pair<K,V> {
 
     public void setValue(V value) {
         this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Pair && ((Pair) obj).key.equals(key) && ((Pair) obj).value.equals(value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key, value);
-    }
-
-    @Override
-    public String toString() {
-        return "[" + key.toString() + "] [" + value.toString() + "]";
     }
 }
